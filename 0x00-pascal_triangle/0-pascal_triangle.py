de5488940a77fd6f93fa84d914f6eea89a923c9b
Javan Otieno <javan.o.otieno@gmail.com>
2023-10-01 22:36:50 +0300

    Inital solution

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module contains the pascal triangle fuction
+"""
+
+
+def pascal_triangle(n: int) -> list[list]:
+    """ Generates a representation of teh pascal's triangle.
+    Args:
+        @n: The number of levels to generate.
+
+    Returns:
+        [] If n < 0 else a list of lists representing the triangle
+
+    """
+    if n < 1:
+        return []
+
+    solution = [[1]]
+    results = [1]
+
+    for i in range(n-1):
+        temp = [0] + results + [0]
+        results = []
+        for j in range(len(temp) - 1):
+            results.append(temp[j] + temp[j+1])
+
+        solution.append(results)
+
+    return solution
---
+++ README.md
@@ -0,0 +1,16 @@
+# ALX Interveiw
+A collection of interview question preparations for the ALX Software Engeneering program
+
+### [Pascal Triangle](./0x00-pascal_triangle/)
+    [1]
+    [1,1]
+    [1,2,1]
+    [1,3,3,1]
+    [1,4,6,4,1]
+
+
+## Author
+Javan Otieno <br>
+[Github](https://www.github.com/Javan-Odhiambo/)
+[LinkedIn](https://www.linkedin.com/ln/javan-otieno/)
+
